0e2796c3d7c8d86b88dcdd5a714cd0052493ab5e
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-03-10 22:27:09 -0300

    Adiciona botão para enviar dados com Id automático

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -45,81 +45,124 @@ class _HomePageState extends State<HomePage> {
         title: Text("Firebase Study"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shadowColor: Colors.yellow,
-                ),
-                onPressed: () async {
-                  //await valueDB.update({
-                  //  "value": "20"
-                  //});
-                  await otherValue
-                      .update({
-                    "value": "99999",
-                    "description": "Updated from App",
-                    "detailsUpdate":"Atualiza campos existentes e cria caso não exista ainda"
-                  })
-                      .then((value) => print("Update OtherValue"))
-                      .catchError((error) => print("Error to connect, $error"));
-
-                  await valueDB
-                      .update({"value": "999", "description": "Updated from App"})
-                      .then((value) => print("Update valueDB"))
-                      .catchError((error) => print("Error to connect, $error"));
-                },
-                child: Row(
-                  children: [
-                    Icon(Icons.send_to_mobile),
-                    Text(
-                      "Update",
-                      style: TextStyle(color: Colors.black),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.yellow,
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              child: Text(_displayValue),
-            ),
-            Container(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shadowColor: Colors.yellow,
-                ),
-                onPressed: () async {
-                  await otherValue
-                      .set({
-                    "value": "50000",
-                    "description": "Sent from App",
-                    "detailsSet":"Define/Seta a base dados com o que está sendo enviado, ignora chaves e valores que existiam antes."
-                  })
-                      .then((value) => print("Set OtherValue"))
-                      .catchError((error) => print("Error to connect, $error"));
+                    onPressed: () async {
+                      //await valueDB.update({
+                      //  "value": "20"
+                      //});
+                      await otherValue
+                          .update({
+                        "value": "99999",
+                        "description": "Updated from App",
+                        "detailsUpdate":"Atualiza campos existentes e cria caso não exista ainda"
+                      })
+                          .then((value) => print("Update OtherValue"))
+                          .catchError((error) => print("Error to connect, $error"));
 
-                  await valueDB
-                      .set({"value": "10", "description": "Sent from App"})
-                      .then((value) => print("Set ValueDB"))
-                      .catchError((error) => print("Error to connect, $error"));
-                },
-                child: Row(
-                  children: [
-                    Icon(Icons.send_to_mobile),
-                    Text(
-                      "Set",
-                      style: TextStyle(color: Colors.black),
+                      await valueDB
+                          .update({"value": "999", "description": "Updated from App"})
+                          .then((value) => print("Update valueDB"))
+                          .catchError((error) => print("Error to connect, $error"));
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.send_to_mobile),
+                        Text(
+                          "Update",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+                Container(
+                  child: Text(_displayValue),
+                ),
+                Container(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.yellow,
+                    ),
+                    onPressed: () async {
+                      await otherValue
+                          .set({
+                        "value": "50000",
+                        "description": "Sent from App",
+                        "detailsSet":"Define/Seta a base dados com o que está sendo enviado, ignora chaves e valores que existiam antes."
+                      })
+                          .then((value) => print("Set OtherValue"))
+                          .catchError((error) => print("Error to connect, $error"));
+
+                      await valueDB
+                          .set({"value": "10", "description": "Sent from App"})
+                          .then((value) => print("Set ValueDB"))
+                          .catchError((error) => print("Error to connect, $error"));
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.send_to_mobile),
+                        Text(
+                          "Set",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.yellow,
+                    ),
+                    onPressed: () async {
+                      await otherValue
+                          .set({
+                        "value": "50000",
+                        "description": "Sent from App",
+                        "detailsSet":"Define/Seta a base dados com o que está sendo enviado, ignora chaves e valores que existiam antes."
+                      })
+                          .then((value) => print("Set OtherValue"))
+                          .catchError((error) => print("Error to connect, $error"));
+
+                      await valueDB
+                          .set({"value": "10", "description": "Sent from App"})
+                          .then((value) => print("Set ValueDB"))
+                          .catchError((error) => print("Error to connect, $error"));
+                    },
+                    child: Row(
+                      children: [
+                        Icon(Icons.edit_attributes_rounded),
+                        Text(
+                          "Enviar dados com Id Automático",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
